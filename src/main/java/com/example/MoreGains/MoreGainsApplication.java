cd0606d6f5b18e4c782debbc82f79e9a15a64193
Fran <fjinfante@profile.es>
2024-06-14 21:38:55 +0200

    init commit

---
+++ src/main/java/com/example/MoreGains/MoreGainsApplication.java
@@ -0,0 +1,13 @@
+package com.example.MoreGains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreGainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoreGainsApplication.class, args);
+	}
+
+}
