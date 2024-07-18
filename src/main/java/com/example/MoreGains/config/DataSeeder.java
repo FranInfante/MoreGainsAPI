10f6cb04c3e291a1f31546d4b9ec85694d80cfc8
Fran <infante.portero@gmail.com>
2024-07-18 20:57:35 +0200

    DataSeeder

--- src/main/java/com/example/MoreGains/config/DataSeeder.java
+++ src/main/java/com/example/MoreGains/config/DataSeeder.java
@@ -33,8 +33,8 @@ public class DataSeeder implements CommandLineRunner {
     private Optional<Users> trainer2;
     private Optional<Users> client1;
 
-    @Override
     @Transactional
+    @Override
     public void run(String... args) throws Exception {
         seedData();
     }
@@ -63,11 +63,61 @@ public class DataSeeder implements CommandLineRunner {
     @Transactional
     private void seedUsers() {
         Users[] users = {
-                Users.builder().username("user1").email("user1@example.com").password("password").privacySetting(PrivacySetting.PUBLIC).build(),
-                Users.builder().username("user2").email("user2@example.com").password("password").privacySetting(PrivacySetting.PUBLIC).build(),
-                Users.builder().username("trainer1").email("trainer1@example.com").password("password").isTrainer(true).privacySetting(PrivacySetting.PRIVATE).build(),
-                Users.builder().username("trainer2").email("trainer2@example.com").password("password").isTrainer(true).privacySetting(PrivacySetting.PRIVATE).build(),
-                Users.builder().username("client1").email("client1@example.com").password("password").privacySetting(PrivacySetting.PUBLIC).build()
+                Users.builder()
+                        .username("user1")
+                        .email("user1@example.com")
+                        .password("password")
+                        .photoUrl("url_to_user1_photo")
+                        .bio("Bio for user1")
+                        .fitnessGoals("Fitness goals for user1")
+                        .isTrainer(false)
+                        .isAvailable(true)
+                        .privacySetting(PrivacySetting.PUBLIC)
+                        .build(),
+                Users.builder()
+                        .username("user2")
+                        .email("user2@example.com")
+                        .password("password")
+                        .photoUrl("url_to_user2_photo")
+                        .bio("Bio for user2")
+                        .fitnessGoals("Fitness goals for user2")
+                        .isTrainer(false)
+                        .isAvailable(true)
+                        .privacySetting(PrivacySetting.PUBLIC)
+                        .build(),
+                Users.builder()
+                        .username("trainer1")
+                        .email("trainer1@example.com")
+                        .password("password")
+                        .photoUrl("url_to_trainer1_photo")
+                        .bio("Bio for trainer1")
+                        .fitnessGoals("Fitness goals for trainer1")
+                        .isTrainer(true)
+                        .isAvailable(true)
+                        .privacySetting(PrivacySetting.PRIVATE)
+                        .build(),
+                Users.builder()
+                        .username("trainer2")
+                        .email("trainer2@example.com")
+                        .password("password")
+                        .photoUrl("url_to_trainer2_photo")
+                        .bio("Bio for trainer2")
+                        .fitnessGoals("Fitness goals for trainer2")
+                        .isTrainer(true)
+                        .isAvailable(true)
+                        .privacySetting(PrivacySetting.PRIVATE)
+                        .build(),
+                Users.builder()
+                        .username("client1")
+                        .email("client1@example.com")
+                        .password("password")
+                        .photoUrl("url_to_client1_photo")
+                        .bio("Bio for client1")
+                        .fitnessGoals("Fitness goals for client1")
+                        .isTrainer(false)
+                        .isAvailable(true)
+                        .privacySetting(PrivacySetting.PUBLIC)
+                        .build()
         };
 
         usersRepository.saveAll(Arrays.asList(users));
@@ -76,18 +126,18 @@ public class DataSeeder implements CommandLineRunner {
     @Transactional
     private void seedMuscleGroups() {
         MuscleGroup[] muscleGroups = {
-                MuscleGroup.builder().name("Legs").build(),
-                MuscleGroup.builder().name("Back").build(),
-                MuscleGroup.builder().name("Chest").build(),
-                MuscleGroup.builder().name("Arms").build(),
-                MuscleGroup.builder().name("Shoulders").build()
+                MuscleGroup.builder().name("Legs").isAvailable(true).build(),
+                MuscleGroup.builder().name("Back").isAvailable(true).build(),
+                MuscleGroup.builder().name("Chest").isAvailable(true).build(),
+                MuscleGroup.builder().name("Arms").isAvailable(true).build(),
+                MuscleGroup.builder().name("Shoulders").isAvailable(true).build()
         };
 
         muscleGroupRepository.saveAll(Arrays.asList(muscleGroups));
     }
 
     @Transactional
-    private void seedExercises() {
+    public void seedExercises() {
         MuscleGroup legs = muscleGroupRepository.findByNameIgnoreCase("Legs");
         MuscleGroup back = muscleGroupRepository.findByNameIgnoreCase("Back");
         MuscleGroup chest = muscleGroupRepository.findByNameIgnoreCase("Chest");
@@ -95,11 +145,41 @@ public class DataSeeder implements CommandLineRunner {
         MuscleGroup shoulders = muscleGroupRepository.findByNameIgnoreCase("Shoulders");
 
         List<Exercise> exercises = Arrays.asList(
-                Exercise.builder().name("Squats").muscleGroup(legs).build(),
-                Exercise.builder().name("Deadlifts").muscleGroup(back).build(),
-                Exercise.builder().name("Bench Press").muscleGroup(chest).build(),
-                Exercise.builder().name("Bicep Curls").muscleGroup(arms).build(),
-                Exercise.builder().name("Shoulder Press").muscleGroup(shoulders).build()
+                Exercise.builder()
+                        .name("Squats")
+                        .description("Lower body exercise targeting quadriceps, hamstrings, and glutes.")
+                        .videoUrl("https://example.com/squats-video")
+                        .isAvailable(true)
+                        .muscleGroup(legs)
+                        .build(),
+                Exercise.builder()
+                        .name("Deadlifts")
+                        .description("Compound exercise involving lifting a barbell off the ground.")
+                        .videoUrl("https://example.com/deadlifts-video")
+                        .isAvailable(true)
+                        .muscleGroup(back)
+                        .build(),
+                Exercise.builder()
+                        .name("Bench Press")
+                        .description("Upper body exercise focusing on chest muscles.")
+                        .videoUrl("https://example.com/bench-press-video")
+                        .isAvailable(true)
+                        .muscleGroup(chest)
+                        .build(),
+                Exercise.builder()
+                        .name("Bicep Curls")
+                        .description("Isolation exercise targeting biceps.")
+                        .videoUrl("https://example.com/bicep-curls-video")
+                        .isAvailable(true)
+                        .muscleGroup(arms)
+                        .build(),
+                Exercise.builder()
+                        .name("Shoulder Press")
+                        .description("Exercise targeting shoulder muscles.")
+                        .videoUrl("https://example.com/shoulder-press-video")
+                        .isAvailable(true)
+                        .muscleGroup(shoulders)
+                        .build()
         );
 
         exerciseRepository.saveAll(exercises);
@@ -107,45 +187,57 @@ public class DataSeeder implements CommandLineRunner {
 
     @Transactional
     private void seedFavorites() {
-        Exercise squat = exerciseRepository.findByNameIgnoreCase("Squats");
 
         user1.ifPresent(u -> {
-            Favorite favorite1 = Favorite.builder().users(u).exercise(squat).build();
+            Exercise squat = exerciseRepository.findByNameIgnoreCase("Squats");
+            Favorite favorite1 = Favorite.builder().users(u).exercise(squat).isAvailable(true).build();
             favoriteRepository.save(favorite1);
         });
 
         user2.ifPresent(u -> {
             Exercise deadlifts = exerciseRepository.findByNameIgnoreCase("Deadlifts");
-            Favorite favorite2 = Favorite.builder().users(u).exercise(deadlifts).build();
+            Favorite favorite2 = Favorite.builder().users(u).exercise(deadlifts).isAvailable(true).build();
             favoriteRepository.save(favorite2);
         });
 
         client1.ifPresent(u -> {
             Exercise benchPress = exerciseRepository.findByNameIgnoreCase("Bench Press");
-            Favorite favorite3 = Favorite.builder().users(u).exercise(benchPress).build();
+            Favorite favorite3 = Favorite.builder().users(u).exercise(benchPress).isAvailable(true).build();
             favoriteRepository.save(favorite3);
         });
 
         trainer1.ifPresent(u -> {
             Exercise bicepCurls = exerciseRepository.findByNameIgnoreCase("Bicep Curls");
-            Favorite favorite4 = Favorite.builder().users(u).exercise(bicepCurls).build();
+            Favorite favorite4 = Favorite.builder().users(u).exercise(bicepCurls).isAvailable(true).build();
             favoriteRepository.save(favorite4);
         });
 
         trainer2.ifPresent(u -> {
             Exercise shoulderPress = exerciseRepository.findByNameIgnoreCase("Shoulder Press");
-            Favorite favorite5 = Favorite.builder().users(u).exercise(shoulderPress).build();
+            Favorite favorite5 = Favorite.builder().users(u).exercise(shoulderPress).isAvailable(true).build();
             favoriteRepository.save(favorite5);
         });
     }
 
     @Transactional
     private void seedMesocycles() {
-        // Assume you have predefined Users objects
         trainer1.ifPresent(trainer -> {
             Mesocycle[] mesocycles = {
-                    Mesocycle.builder().users(trainer).name("Summer Shred").startDate(LocalDate.now()).endDate(LocalDate.now().plusMonths(3)).build(),
-                    Mesocycle.builder().users(trainer).name("Winter Bulk").startDate(LocalDate.now().minusMonths(2)).endDate(LocalDate.now()).build()
+                    Mesocycle.builder()
+                            .users(trainer)
+                            .name("Summer Shred")
+                            .startDate(LocalDate.now()).endDate(LocalDate.now().plusMonths(3))
+                            .description("Summer Shred")
+                            .isAvailable(true)
+                            .build(),
+                    Mesocycle.builder()
+                            .users(trainer)
+                            .name("Winter Bulk")
+                            .startDate(LocalDate.now().minusMonths(2))
+                            .endDate(LocalDate.now())
+                            .description("Winter Bulk")
+                            .isAvailable(true)
+                            .build()
             };
 
             mesocycleRepository.saveAll(Arrays.asList(mesocycles));
@@ -154,17 +246,16 @@ public class DataSeeder implements CommandLineRunner {
 
     @Transactional
     private void seedWorkouts() {
-        // Assume you have predefined Users objects
         client1.ifPresent(client -> {
             Workout[] workouts = {
-                    Workout.builder().users(client).date(LocalDate.now()).name("Leg Day").build(),
-                    Workout.builder().users(client).date(LocalDate.now().minusDays(1)).name("Back and Biceps").build(),
-                    Workout.builder().users(client).date(LocalDate.now().minusDays(2)).name("Chest and Triceps").build()
+                    Workout.builder().users(client).date(LocalDate.now()).name("Leg Day").description("Leg workout focusing on quadriceps, hamstrings, and calves.").isAvailable(true).build(),
+                    Workout.builder().users(client).date(LocalDate.now().minusDays(1)).name("Back and Biceps").description("Back and biceps workout targeting back muscles and biceps.").isAvailable(true).build(),
+                    Workout.builder().users(client).date(LocalDate.now().minusDays(2)).name("Chest and Triceps").description("Chest and triceps workout focusing on chest muscles and triceps.").isAvailable(true).build()
             };
 
             workoutRepository.saveAll(Arrays.asList(workouts));
         });
-    }
+    }º
 
     @Transactional
     private void seedWorkoutExercises() {
